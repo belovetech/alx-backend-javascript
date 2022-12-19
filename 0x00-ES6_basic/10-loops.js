c2c7d68200f9ca9df7b93c0c1d3692ea7281e84c
belovetech <belovetech@gmail.com>
2022-12-19 15:55:13 +0100

    [Feat]: use for...of operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,12 +1,3 @@
-// export default function appendToEachArrayValue(array, appendString) {
-//   for (var idx in array) {
-//     var value = array[idx];
-//     array[idx] = appendString + value;
-//   }
-
-//   return array;
-// }
-
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (let element of array) {
