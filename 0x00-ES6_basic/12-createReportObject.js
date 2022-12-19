a4b785722b12eaf9d333f3d5e9e5d7a178f5e49b
belovetech <belovetech@gmail.com>
2022-12-19 17:21:40 +0100

    [Feat]: create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  let object = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
+    },
+  };
+  return object;
+}
