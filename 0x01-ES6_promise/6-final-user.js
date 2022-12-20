f90e85ca5652afc93be8692d56bcaee831ed9252
belovetech <belovetech@gmail.com>
2022-12-20 21:15:01 +0100

    [update]: return object

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,8 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
-  ]).then((res) => res);
+  ]).then((res) => res.map((item) => ({
+    status: item.status,
+    value: item.value === 'fulfilled' ? item.value : item.reason,
+  })));
 }
