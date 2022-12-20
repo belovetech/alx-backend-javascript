77de63342f4f331eb96bae65f34ec1c2e579091b
belovetech <belovetech@gmail.com>
2022-12-20 21:00:54 +0100

    [Feat]: simple promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,5 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
-  ]).then((res) => {
-    console.log(res);
-  });
+  ]).then((res) => res);
 }
