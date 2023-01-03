4df7e99dc9a5cf119d38bbb1d895909521ebea1a
belovetech <belovetech@gmail.com>
2023-01-03 18:31:32 +0100

    [Update]: take of edge case

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,6 +3,9 @@
  * @param {Map} map - A collection of map
  */
 export default function (map) {
+  if (!map instanceof Map) {
+    throw new Error('Cannot process');
+  }
   map.forEach((value, key, map) => {
     if (value === 1) {
       map.set(key, 100);
