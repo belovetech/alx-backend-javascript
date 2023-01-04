a343b4e58ed65d9b563ccae1e4e92d263cb9a459
belovetech <belovetech@gmail.com>
2023-01-04 21:38:05 +0100

    [Feat]: extend teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,7 @@ interface Teacher {
   [index: string]: any;
 }
 
+// Teacher example
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -15,4 +16,18 @@ const teacher3: Teacher = {
   contract: false,
 };
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Director example
+const director1: Directors = {
+  firstName: 'James',
+  lastName: 'Smith',
+  location: 'USA',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
 console.log(teacher3);
+console.log(director1);
