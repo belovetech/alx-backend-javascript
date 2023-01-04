db17e7acd56f9eb22927faf118929ccd84753cb9
belovetech <belovetech@gmail.com>
2023-01-04 22:05:18 +0100

    [Update]: student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -37,6 +37,34 @@ function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
 
+export interface StudentClassConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass {
+  // Not initialized, but no error
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return `Currently working`;
+  }
+
+  displayName(): string {
+    return this._firstName;
+  }
+}
+
 console.log(printTeacher('Raheem', 'Abeeb'));
 console.log(teacher3);
 console.log(director1);
