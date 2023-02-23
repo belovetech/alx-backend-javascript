398c667676cca32acd37500582e959377604e742
belovetech <belovetech@gmail.com>
2023-02-24 00:23:57 +0100

    [Refactor]: test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function (a, b) {
+describe('calculateNumber', function () {
   it('Should return 4', function () {
     assert.equal(calculateNumber(1, 3), 4);
   });
