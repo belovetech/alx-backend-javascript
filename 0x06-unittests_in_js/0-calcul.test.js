9adab78be39450298ea4ed17211e7c65435eacb2
belovetech <belovetech@gmail.com>
2023-02-24 00:26:05 +0100

    [Refactor]: test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,46 +3,46 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
   it('Should return 4', function () {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('Should return -5', function () {
-    assert.equal(calculateNumber(-3, -2), -5);
+    assert.strictEqual(calculateNumber(-3, -2), -5);
   });
 
   it('Should return 5', function () {
-    assert.equal(calculateNumber(1.4, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.4, 3.7), 5);
   });
 
   it('Should return -5', function () {
-    assert.equal(calculateNumber(-1.4, -3.7), -5);
+    assert.strictEqual(calculateNumber(-1.4, -3.7), -5);
   });
 
   it('Should return 5', function () {
-    assert.equal(calculateNumber(1.45, 3.79), 5);
+    assert.strictEqual(calculateNumber(1.45, 3.79), 5);
   });
 
   it('Should return NaN', function () {
-    assert.equal(calculateNumber('a', 'b'), NaN);
+    assert.strictEqual(calculateNumber('a', 'b'), NaN);
   });
 
   it('Should return NaN', function () {
-    assert.equal(calculateNumber('goat', 'dog'), NaN);
+    assert.strictEqual(calculateNumber('goat', 'dog'), NaN);
   });
 
   it('Should return NaN', function () {
-    assert.equal(calculateNumber([1, 2, 3], 5), NaN);
+    assert.strictEqual(calculateNumber([1, 2, 3], 5), NaN);
   });
 
   it('Should return NaN', function () {
-    assert.equal(calculateNumber({ a: 24 }, 5), NaN);
+    assert.strictEqual(calculateNumber({ a: 24 }, 5), NaN);
   });
 
   it('Should return NaN', function () {
-    assert.equal(calculateNumber(), NaN);
+    assert.strictEqual(calculateNumber(), NaN);
   });
 
   it('Should return NaN', function () {
-    assert.equal(calculateNumber(2), NaN);
+    assert.strictEqual(calculateNumber(2), NaN);
   });
 });
