72996b256d3378fbf41fa3254306769869049aca
belovetech <belovetech@gmail.com>
2023-02-24 00:22:32 +0100

    [Refactor] test cases:

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,78 +2,47 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function (a, b) {
-  it('Should return 4', function (done) {
+  it('Should return 4', function () {
     assert.equal(calculateNumber(1, 3), 4);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return -5', function (done) {
+  it('Should return -5', function () {
     assert.equal(calculateNumber(-3, -2), -5);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return 5', function (done) {
+  it('Should return 5', function () {
     assert.equal(calculateNumber(1.4, 3.7), 5);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return -5', function (done) {
+  it('Should return -5', function () {
     assert.equal(calculateNumber(-1.4, -3.7), -5);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return 5', function (done) {
+  it('Should return 5', function () {
     assert.equal(calculateNumber(1.45, 3.79), 5);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return NaN', function (done) {
+  it('Should return NaN', function () {
     assert.equal(calculateNumber('a', 'b'), NaN);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return NaN', function (done) {
+  it('Should return NaN', function () {
     assert.equal(calculateNumber('goat', 'dog'), NaN);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return NaN', function (done) {
+  it('Should return NaN', function () {
     assert.equal(calculateNumber([1, 2, 3], 5), NaN);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return NaN', function (done) {
+  it('Should return NaN', function () {
     assert.equal(calculateNumber({ a: 24 }, 5), NaN);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return NaN', function (done) {
+  it('Should return NaN', function () {
     assert.equal(calculateNumber(), NaN);
-    done();
   });
-});
 
-describe('calculateNumber', function (a, b) {
-  it('Should return NaN', function (done) {
+  it('Should return NaN', function () {
     assert.equal(calculateNumber(2), NaN);
-    done();
   });
 });
